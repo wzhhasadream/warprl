634c12688bf2e3013e0ac22c70c3a98a85e0262c
wzh <3020210034@tju.edu.cn>
2026-05-29 21:13:28 +0800

    add params

--- nnxrl/model/normailze_params.py
+++ nnxrl/model/normailze_params.py
@@ -4,13 +4,13 @@ import jax
 
 def normalize_linear_kernel(kernel: jax.Array, eps: float = 1e-8) -> jax.Array:
     if kernel.ndim == 2:   # 单个模型
-        axis = 0
+        axis = (0, 1)
     elif kernel.ndim == 3:  # 集成模型
-        axis = 1
+        axis = (1, 2)
     else:
         raise ValueError(f"Unsupported Linear kernel shape: {kernel.shape}")
 
-    norm = jnp.linalg.norm(kernel, ord=2, axis=axis, keepdims=True)
+    norm = jnp.linalg.norm(kernel, axis=axis, keepdims=True)
     return kernel / jnp.maximum(norm, eps)
 
 
@@ -40,15 +40,15 @@ def project_param(module: nnx.Module) -> None:
         if isinstance(m, nnx.Linear):
             m.kernel.value = normalize_linear_kernel(m.kernel.value)
 
-        elif isinstance(m, (nnx.LayerNorm, nnx.BatchNorm)):
-            scale = getattr(m, "scale", None)
-            bias = getattr(m, "bias", None)
-            if scale is not None and bias is not None:
-                scale.value, bias.value = normalize_scale_bias(
-                    scale.value, bias.value
-                )
+        # elif isinstance(m, (nnx.LayerNorm, nnx.BatchNorm)):
+        #     scale = getattr(m, "scale", None)
+        #     bias = getattr(m, "bias", None)
+        #     if scale is not None and bias is not None:
+        #         scale.value, bias.value = normalize_scale_bias(
+        #             scale.value, bias.value
+        #         )
 
-        elif hasattr(nnx, "RMSNorm") and isinstance(m, nnx.RMSNorm):
-            scale = getattr(m, "scale", None)
-            if scale is not None:
-                scale.value = normalize_scale(scale.value)
+        # elif hasattr(nnx, "RMSNorm") and isinstance(m, nnx.RMSNorm):
+        #     scale = getattr(m, "scale", None)
+        #     if scale is not None:
+        #         scale.value = normalize_scale(scale.value)
